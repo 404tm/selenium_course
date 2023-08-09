1d2c43e9524fed6820730e9ded38f54366fd64ca
yarrr_m <404tm@mail.ru>
2023-08-09 22:11:55 +0300

    не работает ничё

--- test_less32.py
+++ test_less32.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
 import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-from selenium_course.function_math import calc
 import time
 
 class test_reg_page:
